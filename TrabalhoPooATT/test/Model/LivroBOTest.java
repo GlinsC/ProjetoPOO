a58758ad065724c6082c3a06dd09b6b7931b904c
Nicolas-abc <179665547+Nicolas-abc@users.noreply.github.com>
2025-12-12 01:33:44 -0300

    Add validation tests for LivroBO class
    - Implementados testes unitários para todos os métodos de validação da classe LivroBO.
    - Cobertura adicionada para:
        -validarAno (ano válido, ano antigo e ano futuro)
        -validarTitulo (válido, vazio e muito curto)
       -validarAutor (válido, vazio e muito curto)
       -validarEditora (válida e vazia)
       -validarCampos (todos válidos e cenário com erro)
    - Padronização do uso de ValidationException em todos os testes.
    - Ajustes de nomenclatura e consistência nos asserts.

--- TrabalhoPooATT/test/Model/LivroBOTest.java
+++ TrabalhoPooATT/test/Model/LivroBOTest.java
@@ -1,6 +1,7 @@
+package Model;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import Model.LivroBO;
 import exception.ValidationException;
 
 public class LivroBOTest {
@@ -19,5 +20,55 @@ public class LivroBOTest {
     void testValidarAnoFuturo() {
         int anoFuturo = java.time.Year.now().getValue() + 1;
         assertThrows(ValidationException.class, () -> LivroBO.validarAno(anoFuturo));
+    }   
+
+    @Test
+    void testeValidarTituloValido(){
+        assertDoesNotThrow(() -> LivroBO.validarTitulo("harry potter"));
     }
+    @Test
+    void testValidarTituloVazio(){
+        assertThrows(ValidationException.class , () -> LivroBO.validarTitulo(""));
+    }
+    @Test
+    void validarTituloCurto(){
+         assertThrows(ValidationException.class , () -> LivroBO.validarTitulo("S"));
+    }
+    @Test
+    void testValidarAutorValido(){
+        assertDoesNotThrow(() -> LivroBO.validarAutor("Machado de Assis"));
+    }
+
+
+    @Test
+    void testValidarAutorVazio(){
+        assertThrows(ValidationException.class, () -> LivroBO.validarAutor(""));
+    }
+
+    @Test
+    void testValidarAutorCurto(){
+        assertThrows(ValidationException.class, () -> LivroBO.validarAutor("N.R"));
+    }
+    @Test
+    void testValidarEditoraValida(){
+        assertDoesNotThrow(() -> LivroBO.validarEditora("Saraiva"));
+    }
+    @Test
+    void testValidarEditoraVazia(){
+         assertThrows(ValidationException.class, () -> LivroBO.validarEditora(""));
+    }
+    @Test 
+    void testValidarCamposValidos(){
+         assertDoesNotThrow(() -> LivroBO.validarCampos("os sertoes","euclides","principis"));
+    }
+    @Test 
+    void testValidarCamposComErro(){
+        assertThrows(ValidationException.class, () -> LivroBO.validarCampos("",//titulo invalido
+                                                                           "autor",
+                                                                          "editora"));
+    }
+    
 }
+
+
+
